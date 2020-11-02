12cfebfb12a294398d27ec3614f48510dadfa4ce
sparrow <ycfn97@gmail.com>
2020-11-02 10:52:54 +0800

    sparkstreaming

--- SparkStreaming_AD/src/main/scala/util/RandomOptions.scala
+++ SparkStreaming_AD/src/main/scala/util/RandomOptions.scala
@@ -1,7 +1,6 @@
 package util
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
-
 /**
  * Copyright(c) 2020-2021 sparrow All Rights Reserved
  * Project: SparkStreaming_AD
@@ -12,32 +11,25 @@ import scala.util.Random
  * @version 1.0
  * @since JDK 1.8
  */
-
 // value值出现的比例，例如：(男，8) (女:2)
 case class RanOpt[T](value: T, weight: Int)
 
-
 object RandomOptions {
   def apply[T](opts: RanOpt[T]*): RandomOptions[T] = {
-
     val randomOptions = new RandomOptions[T]()
-
     for (opt <- opts) {
       // 累积总的权重： 8 + 2
       randomOptions.totalWeight += opt.weight
-
       // 根据每个元素的自己的权重，向buffer中存储数据。权重越多存储的越多
       for (i <- 1 to opt.weight) {
         // 男 男 男 男 男 男 男 男 女 女
         randomOptions.optsBuffer += opt.value
       }
     }
-
     randomOptions
   }
 
   def main(args: Array[String]): Unit = {
-
     for (i <- 1 to 10) {
       println(RandomOptions(RanOpt("男", 8), RanOpt("女", 2)).getRandomOpt)
     }
@@ -45,10 +37,8 @@ object RandomOptions {
 }
 
 class RandomOptions[T](opts: RanOpt[T]*) {
-
   var totalWeight = 0
   var optsBuffer = new ListBuffer[T]
-
   def getRandomOpt: T = {
     // 随机选择：0-9
     val randomNum: Int = new Random().nextInt(totalWeight)
